a54196958fd21b39b5fd82bc1c705e844e685617
Utsavshrma <princu419@gmail.com>
2024-12-18 22:22:12 +0530

    t push -u origin main-- A new project  started
    	--INSERT--

---
+++ src/componenets/core/HomePage/TimelineSection.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const TimelineSection = ( ) => {
+  return (
+    <div>
+    <div className=" flex flex-row gap-15 items-center">
+
+    </div>
+
+    </div>
+  );
+};
+
+export default TimelineSection;
